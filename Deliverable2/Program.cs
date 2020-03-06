222f76c6616de38cbc8f6ee8d574a382bb29a056
A_Wilson <dreamy26@icloud.com>
2020-03-05 23:42:27 -0500

    initial commit

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class MainClass
+    {
+        private static char checksum;
+
+        public static void Main(string[] args)
+        {
+            string input = "";
+            string message = "";
+            int checkSum = 0;
+
+           
+            {
+                {
+
+
+                    Console.WriteLine("So you want to send a coded message?");
+                    input = Console.ReadLine().ToUpper();
+
+
+                    foreach (char letter in input)
+                    {
+                        string messageLetter = (letter - 64).ToString();
+                       
+                        message += messageLetter + "-";
+                       
+                        checksum = checksum + letter;
+                    }
+
+                    string value = $"Your Message checksum is {checksum}";
+                    Console.WriteLine(value);
+                    Console.WriteLine($"Your Encoded message is {message}");
+                    Console.ReadLine();
+                }
+            }
+
+
+        }
+    }
+}
